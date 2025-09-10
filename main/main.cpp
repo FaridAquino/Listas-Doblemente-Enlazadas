26a2ba2e3a021459df964b7821f86d9be30df151
R10-Mat <rayhan.matos@utec.edu.pe>
2025-09-10 09:52:07 -0500

    Funciones agregadas

--- main/main.cpp
+++ main/main.cpp
@@ -38,8 +38,7 @@ public:
     } 
 
     void push_front(T dato){
-      Nodo<T>* new_nodo = new Nodo<T>(dato);
-      
+      auto new_nodo = new Nodo<T>(dato);
       new_nodo->next = sentinela->next;
       new_nodo->preview = sentinela;
       sentinela->next->prev = new_nodo;
@@ -48,20 +47,74 @@ public:
     }  
 
     void push_back(T dato){
-      Nodo<T>* new_nodo =new Nodo<T>(dato);
+      auto new_nodo =new Nodo<T>(dato);
       new_nodo->preview = sentinela->preview;
       new_nodo->next = sentinela;
       sentinela->preview->next = new_nodo;
       sentinela->preview = new_nodo;
     }  
 
-    T pop_front(){}  //Derek
+    T pop_front() {
+        if (sentinela->next == sentinela) {
+            throw("La lista esta vacia");
+        }
+        Nodo<T>* first_nodo = sentinela->next;
+        Nodo<T>* temp = first_nodo->next;
+        sentinela->next = temp;
+        temp->preview = sentinela;
+        delete first_nodo;
+        return sentinela;
+    }
 
-    T pop_back(){}  //Derek
+    T pop_back() {
+        if (sentinela->next == sentinela) {
+            throw("La lista esta vacia");
+        }
+        Nodo<T>* lastnode = sentinela->preview;
+        Nodo<T>* temp = lastnode->preview;
+        sentinela->preview = temp;
+        temp->next = sentinela;
+        delete lastnode;
+        return sentinela;
+    }
 
-    void insert(T, int){} //Derek
+    void insert(T nuevo_dato, int index) {
+        auto new_nodo = new Nodo<T>(nuevo_dato);
+        Nodo<T>* temp = sentinela;
+        for (int i = 0; i < index; i++) {
+            if (temp == sentinela) {
+                cout << "Index no válido" << endl;
+                break;
+            }
+            if (i == index-1) {
+                Nodo<T>* temp2 = temp->next;
+                temp->next = new_nodo;
+                new_nodo->preview = temp;
+                new_nodo->next = temp2;
+                temp2->preview = new_nodo;
+                break;
+            }
+            temp = temp->next;
+        }
+    } //Derek
 
-    void remove(int){} //Derek
+    void remove(int index) {
+        Nodo<T>* temp = sentinela->next;
+        for (int i = 0; i <= index; i++) {
+            if (temp == sentinela) {
+                cout << "Index no válido" << endl;
+                break;
+            }
+            if (i == index) {
+                Nodo<T>* temp2 = temp->next;
+                temp->preview->next = temp2;
+                temp2->preview = temp->preview;
+                delete temp;
+                break;
+            }
+            temp = temp->next;
+        }
+    }
 
     T operator[](int indice) {
         auto* p1=sentinela->next;
@@ -135,6 +188,10 @@ public:
 };
 
 int main() {
+
+
+
+
 
     return 0;
 }
